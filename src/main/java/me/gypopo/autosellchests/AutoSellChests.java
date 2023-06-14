7c3910b8e5ca4f6eb0a2b07eeccec1d48397e23c
BuildTools <ferreschillemans@gmail.com>
2023-06-14 16:39:08 +0200

    v2.4.1 - Now requires atleast EconomyShopGUI v5.5.1/v4.10.2

--- src/main/java/me/gypopo/autosellchests/AutoSellChests.java
+++ src/main/java/me/gypopo/autosellchests/AutoSellChests.java
@@ -64,8 +64,8 @@ public final class AutoSellChests extends JavaPlugin implements Listener {
 
             premium = false;
             int version = Integer.parseInt(Bukkit.getServer().getPluginManager().getPlugin("EconomyShopGUI").getDescription().getVersion().split("-")[0].replace(".", ""));
-            if (version < 544) {
-                this.getLogger().warning("This plugin requires a newer version of EconomyShopGUI, please download version v5.4.4 or later, disabling...");
+            if (version < 551) {
+                this.getLogger().warning("This plugin requires a newer version of EconomyShopGUI, please download version v5.5.1 or later, disabling...");
                 this.getServer().getPluginManager().disablePlugin(this);
                 return;
             }
@@ -75,8 +75,8 @@ public final class AutoSellChests extends JavaPlugin implements Listener {
 
             premium = true;
             int version = Integer.parseInt(Bukkit.getServer().getPluginManager().getPlugin("EconomyShopGUI-Premium").getDescription().getVersion().split("-")[0].replace(".", ""));
-            if (version < 490) {
-                this.getLogger().warning("This plugin requires a newer version of EconomyShopGUI Premium, please download version v4.9.0 or later, disabling...");
+            if (version < 4102) {
+                this.getLogger().warning("This plugin requires a newer version of EconomyShopGUI Premium, please download version v4.10.2 or later, disabling...");
                 this.getServer().getPluginManager().disablePlugin(this);
                 return;
             }
